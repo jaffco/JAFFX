3c25571ddf80f704a1aaf9cd81050c4634e955e2
Joel Jaffe <joel@jaffesd.com>
2024-11-19 17:04:56 -0800

    Improved `loadMeter`

--- src/loadMeter/loadMeter.cpp
+++ src/loadMeter/loadMeter.cpp
@@ -4,7 +4,7 @@ struct LoadMeter : Jaffx::Program {
   CpuLoadMeter loadMeter;
 
   void init() override {
-    hardware.StartLog();
+    hardware.StartLog(true);
     loadMeter.Init(hardware.AudioSampleRate(), hardware.AudioBlockSize());
   }
 
@@ -24,12 +24,12 @@ struct LoadMeter : Jaffx::Program {
     const float maxLoad = loadMeter.GetMaxCpuLoad();
     const float minLoad = loadMeter.GetMinCpuLoad();
     // print it to the serial connection (as percentages)
-    hardware.PrintLine("Processing Load %%:");
-    hardware.PrintLine("Max: " FLT_FMT3, FLT_VAR3(maxLoad * 100.0f));
-    hardware.PrintLine("Avg: " FLT_FMT3, FLT_VAR3(avgLoad * 100.0f));
-    hardware.PrintLine("Min: " FLT_FMT3, FLT_VAR3(minLoad * 100.0f));
+    hardware.PrintLine("Processing Load:");
+    hardware.PrintLine("Max: " FLT_FMT3 "%%", FLT_VAR3(maxLoad * 100.0f));
+    hardware.PrintLine("Avg: " FLT_FMT3 "%%", FLT_VAR3(avgLoad * 100.0f));
+    hardware.PrintLine("Min: " FLT_FMT3 "%%", FLT_VAR3(minLoad * 100.0f));
     // don't spam the serial connection too much
-    System::Delay(500);
+    System::Delay(1000);
   }
 
 };
