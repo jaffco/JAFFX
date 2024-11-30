7b0460fcbc12fd5a910eff30b6f493dc97bbf02a
Joel Jaffe <joel@jaffesd.com>
2024-11-30 11:25:20 -0800

    Updated `src/settings`

--- src/settings/settings.cpp
+++ src/settings/settings.cpp
@@ -1,6 +1,7 @@
 #include "../../Jaffx.hpp"
 
-// Program that implements Persistent Storage, allowing for the saving of presets between boot cycles
+// Program that implements Persistent Storage, 
+// allowing for the saving of presets between boot cycles
 
 // inspired by:
 // https://forum.electro-smith.com/t/saving-values-to-flash-memory-using-persistentstorage-class-on-daisy-pod/4306
@@ -15,39 +16,36 @@ struct Settings {
 };
 
 struct SettingsTest : Jaffx::Program {
-  bool ledState = false;
-  bool triggered = false;
-	PersistentStorage<Settings> SavedSettings{this->hardware.qspi}; // PersistantStorage for Settings
-	Settings DefaultSettings = {false}; // local instance of Settings
+  bool trigger = false;
+	PersistentStorage<Settings> savedSettings{this->hardware.qspi}; // PersistentStorage for Settings
+	Settings localSettings = {false}; // local settings, constructed with a default
 	
 	// Get stored settings and write to local
 	void loadSettings() {
-		// Reference to local copy of settings stored in flash
-		Settings &LocalSettings = this->SavedSettings.GetSettings();
-		this->ledState = LocalSettings.ledState; // update local var
+		localSettings = savedSettings.GetSettings();
 	}
 
 	// Get local settings and write to storage
 	void saveSettings() {
-		// Reference to local copy of settings stored in flash
-		Settings &LocalSettings = this->SavedSettings.GetSettings();
-		LocalSettings.ledState = !LocalSettings.ledState; // update saved var
-		this->SavedSettings.Save(); // save to flash
+		this->savedSettings.GetSettings() = localSettings; // update with local
+		this->savedSettings.Save(); // save to persistent memory 
 	}
 
 	void init() override {
-		this->SavedSettings.Init(DefaultSettings); // init with default settings
+		this->savedSettings.Init(localSettings); // init with default settings
 		this->loadSettings(); // loadSettings from flash
+		this->localSettings.ledState = !localSettings.ledState; // flip ledState
 		this->saveSettings(); // saveSettings to flash
-		this->triggered = true; // trigger led update
+		this->trigger = true; // trigger led update
 	}
 
 	void loop() override {
-		if (triggered) { // trigger once per boot cycle
-			this->hardware.SetLed(this->ledState);
-			this->triggered = false;
+		if (trigger) { // triggered once per boot cycle
+			this->hardware.SetLed(localSettings.ledState);
+			this->trigger = false;
 		}
 	}
+
 };
 
 int main(void) {
