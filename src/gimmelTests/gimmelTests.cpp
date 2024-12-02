5a921caae63be9af7b0a3e8a0ac4ceb9b2cc84f9
Joel Jaffe <joel@jaffesd.com>
2024-12-02 10:02:56 -0800

    Prototyped container for giml fx in `src/gimmelTests`

--- src/gimmelTests/gimmelTests.cpp
+++ src/gimmelTests/gimmelTests.cpp
@@ -2,6 +2,10 @@
 #include "../../Gimmel/include/gimmel.hpp"
 #include <memory>
 
+// 2024-12-02
+// prototyped containerization of giml fx
+// seems to be breaking delay-based effects
+
 // Testing ground for Gimmel effects
 // Biquad: working!
 // Chorus: working!
@@ -12,32 +16,36 @@
 // Tremolo: working!
 
 struct GimmelTests : Jaffx::Program {
-	std::unique_ptr<giml::Biquad<float>> mBiquad;
-	std::unique_ptr<giml::Chorus<float>> mChorus;
-	std::unique_ptr<giml::Compressor<float>> mCompressor;
-	std::unique_ptr<giml::Delay<float>> mDelay;
-	std::unique_ptr<giml::Detune<float>> mDetune;
-	std::unique_ptr<giml::Reverb<float>> mReverb;
-	std::unique_ptr<giml::Tremolo<float>> mTremolo;
+	std::unique_ptr<giml::Effect<float>> fxChain[3];
 	
 	void init() override {
-		mReverb = std::make_unique<giml::Reverb<float>>(this->samplerate);
-		mReverb->setParams(0.02f, 0.2f, 0.5f, 10.f, 0.9f);
-		mReverb->enable();
+		fxChain[0] = std::make_unique<giml::Reverb<float>>(this->samplerate);
+		auto& mReverb = static_cast<giml::Reverb<float>&>(*fxChain[0]);
+		mReverb.setParams(0.02f, 0.2f, 0.2f, 25.f, 0.3f);
+		mReverb.enable();
 
-		mCompressor = std::make_unique<giml::Compressor<float>>(this->samplerate);
-		mCompressor->setAttack(3.5f);
-		mCompressor->setKnee(4.f);
-		mCompressor->setMakeupGain(20.f);
-		mCompressor->setRatio(14.f);
-		mCompressor->setRelease(100.f);
-		mCompressor->setThresh(-35.f);
-		mCompressor->enable();
+		fxChain[1] = std::make_unique<giml::Delay<float>>(this->samplerate);
+		auto& mDelay = static_cast<giml::Delay<float>&>(*fxChain[0]);
+		mDelay.setDelayTime(398);
+		mDelay.setDamping(0.5f);
+		mDelay.setFeedback(0.3);
+		mDelay.enable();
+
+		fxChain[2] = std::make_unique<giml::Compressor<float>>(this->samplerate);
+		auto& mCompressor = static_cast<giml::Compressor<float>&>(*fxChain[1]);
+		mCompressor.setAttack(3.5f);
+		mCompressor.setKnee(4.f);
+		mCompressor.setMakeupGain(20.f);
+		mCompressor.setRatio(14.f);
+		mCompressor.setRelease(100.f);
+		mCompressor.setThresh(-35.f);
+		mCompressor.enable();
 	}
 
 	float processAudio(float in) override {
-		float out = mReverb->processSample(in);
-		return mCompressor->processSample(giml::powMix<float>(in, out));
+		float out = in;
+		for (auto &e : fxChain) {out = e->processSample(out);}
+		return out;
 	}
 
 };
