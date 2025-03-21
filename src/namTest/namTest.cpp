2c98454886dcdf0198cb2067a80a5eaf569e097b
Joel Jaffe <joel@jaffesd.com>
2025-03-21 15:22:42 -0700

    Added giml fx... I think we've got something here!

--- src/namTest/namTest.cpp
+++ src/namTest/namTest.cpp
@@ -3,23 +3,35 @@
 #include "model.h"
 #include "../../Jaffx.hpp"
 
+#ifndef M_PI
+#define M_PI (3.14159265358979323846)
+#endif
+#ifndef M_E
+#define M_E (2.71828182845904523536)
+#endif
+#ifndef M_PI_2
+#define M_PI_2 (M_PI / 2)
+#endif
+#include "../../Gimmel/include/detune.hpp"
+#include "../../Gimmel/include/delay.hpp"
+#include <memory> // for unique_ptr && make_unique
+
 struct NAMMathsProvider
 {
 #if RTNEURAL_USE_EIGEN
-    template <typename Matrix>
-    static auto tanh (const Matrix& x)
-    {
-        // See: math_approx::tanh<3>
-        const auto x_poly = x.array() * (1.0f + 0.183428244899f * x.array().square());
-        return x_poly.array() * (x_poly.array().square() + 1.0f).array().rsqrt();
-        //return x.array().tanh(); // Tried using Eigen's built in tanh(), also works, failed on the same larger models as above custom tanh
-    }
+  template <typename Matrix>
+  static auto tanh (const Matrix& x) {
+    // See: math_approx::tanh<3>
+    const auto x_poly = x.array() * (1.0f + 0.183428244899f * x.array().square());
+    return x_poly.array() * (x_poly.array().square() + 1.0f).array().rsqrt();
+    //return x.array().tanh(); 
+    // Tried using Eigen's built in tanh(), also works, failed on the same larger models as above custom tanh
+  }
 #elif RTNEURAL_USE_XSIMD
-    template <typename T>
-    static T tanh (const T& x)
-    {
-        return math_approx::tanh<3> (x);
-    }
+  template <typename T>
+  static T tanh (const T& x) {
+    return math_approx::tanh<3> (x);
+  }
 #endif
 };
 
@@ -40,14 +52,37 @@ void loadModel(ModelWeights& model) {
 
 class NamTest : public Jaffx::Firmware {
   ModelWeights model;
+  std::unique_ptr<giml::Detune<float>> mDetune;
+  std::unique_ptr<giml::Delay<float>> mDelay;
+  std::unique_ptr<giml::Delay<float>> mDelay2;
+  giml::EffectsLine<float> mFxChain;
 
   void init() override {
     loadWeights(model);
     loadModel(model);
+
+    mDetune = std::make_unique<giml::Detune<float>>(this->samplerate);
+    mDetune->setParams(0.995f);
+    //mDetune->enable();
+    mFxChain.pushBack(mDetune.get());
+
+    mDelay = std::make_unique<giml::Delay<float>>(this->samplerate);
+    mDelay->setParams(398.f, 0.3f, 0.7f, 0.24f);
+    mDelay->enable();
+    mFxChain.pushBack(mDelay.get());
+
+    mDelay2 = std::make_unique<giml::Delay<float>>(this->samplerate);
+    mDelay2->setParams(798.f, 0.2f, 0.7f, 0.24f);
+    mDelay2->enable();
   }
 
   float processAudio(float in) override {
-    return rtneural_wavenet.forward(in);
+    float dry = rtneural_wavenet.forward(in);
+    float wet = mDetune->processSample(dry);
+    float delay1 = mDelay->processSample(wet);
+    float delay2 = mDelay2->processSample(wet);
+    wet = giml::linMix(delay1, delay2, 0.5f);
+    return wet;
   }
 
   void loop() override {}
