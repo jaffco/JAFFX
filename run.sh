16cb9b5412f5cbd94296531999f67acdf57de5d4
Joel A. Jaffe <joel@jaffesd.com>
2025-08-14 13:51:01 -0700

    Updated `run.sh`

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <path_to_script>"
+  echo "Usage: $0 <path/to/srcFile.cpp>"
   exit 1
 fi
 
 FILEPATH=$(realpath "$1")
+FILEPATH=$(echo "$FILEPATH" | sed 's|\\|/|g') # Convert Windows paths to Unix paths
 DIRPATH=$(dirname "$FILEPATH")
 
 cd "$DIRPATH" || { echo "Error: Could not change directory to $FILEPATH"; exit 1; }
@@ -14,5 +15,3 @@ make clean ; make; make program-dfu
 
 # https://github.com/electro-smith/DaisyWiki/wiki/1.-Setting-Up-Your-Development-Environment#4a-flashing-the-daisy-via-usb
 echo "Ignore Error 74 if related to download get_status"
-
-
